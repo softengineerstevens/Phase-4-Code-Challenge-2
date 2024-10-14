79aed10b0f766f309641b577b5cc3d55ea202561
softengineerstevens <softengineerstevens@gmail.com>
2024-10-14 06:46:42 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -25,5 +25,54 @@ def index():
     return '<h1>Code challenge</h1>'
 
 
+from flask import Flask, jsonify, request, abort
+from models import db, Restaurant, Pizza, RestaurantPizza
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.to_dict() for restaurant in restaurants])
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        return jsonify(restaurant.to_dict())
+    return jsonify({"error": "Restaurant not found"}), 404
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204
+    return jsonify({"error": "Restaurant not found"}), 404
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.to_dict() for pizza in pizzas])
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        new_rp = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+        return jsonify(new_rp.to_dict()), 201
+    except ValueError as e:
+        return jsonify({"errors": [str(e)]}), 400
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
